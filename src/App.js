f7d2847c79540b845a87c98b162d1ccbe818eeaa
Bhavya <kshubhamm@gmail.com>
2019-09-21 16:44:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
+import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div>Hello World!</div>
+//   );
+// }
+
+export default class App extends Component{
+
+  constructor(){
+    super();
+
+    this.state = {
+      value:'',
+      user:[]
+    }
+  }
+
+  setValue = (event)=>{
+    let _value = event.target.value;
+    this.setState({value:_value});
+  }
+
+  includeData=(event)=>{
+
+    if(['Enter','Tab',','].includes(event.key)){
+      event.preventDefault();
+
+      let _users = this.state.value.trim();
+
+      if(_users){
+        this.setState({user:[...this.state.user,_users],value:''})
+      }
+    }
+  }
+
+  delData=(event)=>{
+    let _element = event.target.parentElement.childNodes[0].value;
+
+    let _array = this.state.user.filter(data=>data !== _element);
+
+    this.setState({user:_array})
+  }
+
+  render(){
+    return(
+      <div className="tags-input" data-name="tags-input">
+          {this.state.user.map(data=>
+            <span className="tags">
+              <input type="hidden" value={data} />{data}
+
+              <span className="close" onClick={this.delData}>
+              </span>
+            </span>)}
+
+          <input className="main-tag"
+                  type="text"
+                  value = {this.state.value}
+                  onChange = {this.setValue}
+                  onKeyDown = {this.includeData}
+          />
+      </div>
+    );
+  }
 }
 
-export default App;
+//export default App;
